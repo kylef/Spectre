66e8c29f46e4b87088354ca42692893ea6e369d6
Rahul Katariya <rahulkatariya@me.com>
2016-12-25 22:24:06 +0530

    Add Carthage Support

--- Spectre.playground/Contents.swift
+++ Spectre.playground/Contents.swift
@@ -1,23 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import Spectre
+
 struct Person : CustomStringConvertible {
-  let name:String
-
-  init(name:String) {
-    self.name = name
-  }
-
-  var description:String {
-    return name
-  }
+    let name:String
+    
+    init(name:String) {
+        self.name = name
+    }
+    
+    var description:String {
+        return name
+    }
 }
 
 describe("a person") {
-  let person = Person(name: "Kyle")
-
-  $0.it("has a name") {
-    try expect(person.name) == "Katie"
-  }
-
-  $0.it("returns the name as description") {
-    try expect(person.description) == "Kyle"
-  }
+    let person = Person(name: "Kyle")
+    
+    $0.it("has a name") {
+        try expect(person.name) == "Katie"
+    }
+    
+    $0.it("returns the name as description") {
+        try expect(person.description) == "Kyle"
+    }
 }
+
--- Spectre.playground/Sources/Context.swift
+++
@@ -1,1 +0,0 @@
-../../Sources/Context.swift

8966ec23e124bca6ded4617c166c8085d38a237e
Roman Podymov <podymfrombryansk@yandex.ru>
2020-04-12 20:41:56 +0200

    preparations for Xcode project

---
+++ Sources/Spectre/Spectre.h
@@ -0,0 +1,19 @@
+//
+//  Spectre.h
+//  Spectre
+//
+//  Created by Roman Podymov on 12/04/2020.
+//  Copyright © 2020 Kyle Fuller. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Spectre.
+FOUNDATION_EXPORT double SpectreVersionNumber;
+
+//! Project version string for Spectre.
+FOUNDATION_EXPORT const unsigned char SpectreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Spectre/PublicHeader.h>
+
+

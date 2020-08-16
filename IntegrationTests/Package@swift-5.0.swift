5a7849cb9884915e2014dbb629433e3a707acdb0
Kyle Fuller <kyle@fuller.li>
2020-08-16 16:24:50 +0100

    chore: support Swift 5 for integration tests

---
+++ IntegrationTests/Package@swift-5.0.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+
+let package = Package(
+  name: "SpectreIntegration",
+  products: [
+    .executable(name: "Passing", targets: ["Passing"]),
+    .executable(name: "Disabled", targets: ["Disabled"]),
+    .executable(name: "Failing", targets: ["Failing"]),
+  ],
+  targets: [
+    .target(name: "Spectre", exclude: ["XCTest@4.swift"]),
+    .target(name: "Passing", dependencies: ["Spectre"]),
+    .target(name: "Disabled", dependencies: ["Spectre"]),
+    .target(name: "Failing", dependencies: ["Spectre"]),
+  ],
+  swiftLanguageVersions: [.v5]
+)

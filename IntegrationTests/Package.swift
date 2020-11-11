af24bbe49683de2742f683eb77b1f551cd3c2ea2
Elliott Williams <emw@yelp.com>
2020-11-10 16:27:20 -0800

    Fix integration tests by removing @5 files altogether

--- IntegrationTests/Package.swift
+++ IntegrationTests/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     .executable(name: "Failing", targets: ["Failing"]),
   ],
   targets: [
-    .target(name: "Spectre", exclude: ["Spectre/XCTest@5.swift"]),
+    .target(name: "Spectre"),
     .target(name: "Passing", dependencies: ["Spectre"]),
     .target(name: "Disabled", dependencies: ["Spectre"]),
     .target(name: "Failing", dependencies: ["Spectre"]),
--- Sources/Spectre/XCTest.swift
+++ Sources/Spectre/XCTest.swift
@@ -29,4 +29,15 @@ class XcodeReporter: ContextReporter {
   func addSuccess(_ name: String)  {}
 
   func addDisabled(_ name: String) {}
+
+  func addFailure(_ name: String, failure: FailureType) {
+    // Xcode 12 removed `recordFailure` and replaced with `record(_:)`
+#if compiler(>=5.3) && os(macOS)
+    let location = XCTSourceCodeLocation(filePath: failure.file, lineNumber: failure.line)
+    let issue = XCTIssue(type: .assertionFailure, compactDescription: "\(name): \(failure.reason)", detailedDescription: nil, sourceCodeContext: .init(location: location), associatedError: nil, attachments: [])
+    testCase.record(issue)
+#else
+    testCase.recordFailure(withDescription: "\(name): \(failure.reason)", inFile: failure.file, atLine: failure.line, expected: false)
+#endif
+  }
 }

14ecc6d6853c0eca70fd632fa16f7f043b8392a3
Elliott Williams <emw@yelp.com>
2020-11-11 12:31:52 -0800

    XCTIssue is apparently unavailable when building with Xcode

--- Sources/Spectre/XCTest.swift
+++ Sources/Spectre/XCTest.swift
@@ -32,15 +32,23 @@ class XcodeReporter: ContextReporter {
 
   func addFailure(_ name: String, failure: FailureType) {
     // Xcode 12 removed `recordFailure` and replaced with `record(_:)`
-#if swift(>=4.2)
-  // The `compiler` statement was added in swift 4.2, so it needs to be in a separate statement.
-#if compiler(>=5.3) && os(macOS)
+    #if swift(>=4.2)
+    // The `compiler` statement was added in swift 4.2, so it needs to be in a separate statement to retain
+    // compatibility with 4.x.
+    #if compiler(>=5.3) && os(macOS)
     let location = XCTSourceCodeLocation(filePath: failure.file, lineNumber: failure.line)
+    #if Xcode
+    // As of Xcode 12.0.1, XCTIssue is unavailable even though it is documented:
+    //   https://developer.apple.com/documentation/xctest/xctissue
+    // When building with `swift build`, it is available. Perhaps the xctest overlay behaves differently between the two.
+    let issue = XCTIssueReference(type: .assertionFailure, compactDescription: "\(name): \(failure.reason)", detailedDescription: nil, sourceCodeContext: .init(location: location), associatedError: nil, attachments: [])
+    #else
     let issue = XCTIssue(type: .assertionFailure, compactDescription: "\(name): \(failure.reason)", detailedDescription: nil, sourceCodeContext: .init(location: location), associatedError: nil, attachments: [])
+    #endif
     testCase.record(issue)
-#else
+    #else
     testCase.recordFailure(withDescription: "\(name): \(failure.reason)", inFile: failure.file, atLine: failure.line, expected: false)
-#endif
-#endif
+    #endif
+    #endif
   }
 }
